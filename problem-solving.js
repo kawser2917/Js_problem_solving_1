ddfea524d708dce0879f92d5b4ce55b299b18010
= Md. Kawser Ahmed <kawserahmed8492@gmail.com>
2023-09-24 12:52:43 +0600

    indentation

--- problem-solving.js
+++ problem-solving.js
@@ -1,21 +1,21 @@
-function cubeNumber(number){
-    if(typeof number !== "number"){
+function cubeNumber(number) {
+    if (typeof number !== "number") {
         return "Please Enter a Number";
     }
-    else{
-        const cube = Math.pow(number,3);
+    else {
+        const cube = Math.pow(number, 3);
         return cube;
     }
 }
 
 function matchFinder(string1, string2) {
-    if(typeof string1!=="string" && typeof string2!=="string"){
+    if (typeof string1 !== "string" && typeof string2 !== "string") {
         console.log("Please Enter String")
     }
-    else if( string1.includes(string2) === true){
+    else if (string1.includes(string2) === true) {
         return true;
     }
-    else{
+    else {
         return false;
     }
 }
@@ -23,50 +23,49 @@ function matchFinder(string1, string2) {
 
 function sortMaker(arr) {
     const temp = arr[0]
-    for (let i=0;i<arr.length;i++){
-        if(arr[i] < 0){
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] < 0) {
             return "Invalid Input"
         }
     }
-    if(arr[0] < arr[1]){
+    if (arr[0] < arr[1]) {
         arr[0] = arr[1]
         arr[1] = temp
         return arr
     }
-    else{
+    else {
         return 'equal'
     }
 }
 
 function findAddress(obj) {
-    if (Object.hasOwn(obj,"street") === true && Object.hasOwn(obj,"house") === true && Object.hasOwn(obj,"society") === true){
+    if (Object.hasOwn(obj, "street") === true && Object.hasOwn(obj, "house") === true && Object.hasOwn(obj, "society") === true) {
         return obj.street + "," + obj.house + "," + obj.society;
     }
-    else if (Object.hasOwn(obj,"street") === true && Object.hasOwn(obj,"house") === false && Object.hasOwn(obj,"society") === true){
+    else if (Object.hasOwn(obj, "street") === true && Object.hasOwn(obj, "house") === false && Object.hasOwn(obj, "society") === true) {
         return obj.street + "," + '__' + "," + obj.society;
     }
-    else if (Object.hasOwn(obj,"street") === true && Object.hasOwn(obj,"house") === false && Object.hasOwn(obj,"society") === false){
+    else if (Object.hasOwn(obj, "street") === true && Object.hasOwn(obj, "house") === false && Object.hasOwn(obj, "society") === false) {
         return obj.street + "," + '__' + "," + '__';
     }
 }
 
 function canPay(changeArray, totalDue) {
-    if(changeArray.length<=0){
+    if (changeArray.length <= 0) {
         return "Empty array not allowed"
     }
-    else{
+    else {
         let sum = 0;
-        for(let i = 0;i<changeArray.length;i++){
-            sum = sum+changeArray[i];
+        for (let i = 0; i < changeArray.length; i++) {
+            sum = sum + changeArray[i];
         }
-        if (sum >= totalDue){
+        if (sum >= totalDue) {
             return true;
         }
-        else{
+        else {
             return false;
         }
     }
 }
 
 
- 
